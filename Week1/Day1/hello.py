2c05c400b1da0ec03a153baec44288ef1915b1c3
thanhhuong.cao.thi <thanhhuong.cao.thi@shopee.com>
2025-12-10 22:17:25 +0700

    Initial commit - Week1/Day1 structure

---
+++ Week1/Day1/hello.py
@@ -0,0 +1,2 @@
+print("Hello 清香!")			
+print("Tôi là QA đang học Python Automation!")
---
+++ Week1/Day1/intro.py
@@ -0,0 +1,4 @@
+name = "Qingxiang"			
+goal = "Trở thành QA Automation trong 4 tháng"			
+hours = 1.5			
+print(f"Xin chào, tôi là {name}. Mục tiêu của tôi: {goal}. Mỗi ngày tôi học {hours} giờ.")
